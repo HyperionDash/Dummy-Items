f34d4527c204e8f9e9fd9db6088bf4fb0a4fdda1
HyperionDash <maxbatty404@gmail.com>
2025-07-31 01:00:15 +0100

    Initial commit

---
+++ src/main/java/net/hyper/dummyitems/DummyItemsClient.java
@@ -0,0 +1,10 @@
+package net.hyper.dummyitems;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DummyItemsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
